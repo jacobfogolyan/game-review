042a4d606494399bae2fa47ab50f8420c3182b42
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-08 00:07:31 +1000

    chore: add main

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env.DATABASE_URL, 'hey');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
