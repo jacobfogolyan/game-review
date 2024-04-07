757b6817af27e04dfb6e37691ad80ec2c9449aa1
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-08 09:42:21 +1000

    update eslintrc for frontend

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -1,11 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/library.js"],
   parser: "vue-eslint-parser",
-  // parserOptions: {
-  //   project: true,
-  // },
   parserOptions: {
     project: true,
     extraFileExtensions: [".vue"],
