92368786c99ee596900b0798c4fa0953b3a43dd1
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-08 00:10:55 +1000

    add .env support

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env.DATABASE_URL, 'hey');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
