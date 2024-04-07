fad9940d31e99bed126f235567ed26611067018d
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-08 01:32:25 +1000

    rename eslint to cjs

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -3,7 +3,11 @@ module.exports = {
   root: true,
   extends: ["@repo/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
+  // parserOptions: {
+  //   project: true,
+  // },
   parserOptions: {
     project: true,
+    extraFileExtensions: [".vue"],
   },
 };
