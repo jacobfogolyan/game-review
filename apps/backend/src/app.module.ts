b1f900f7a41d1fb506cd6b611d015b5e1b7ca519
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-13 09:55:03 +1000

    remove demo module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,15 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { GamesModule } from './games/games.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { DingusModule } from './dingus/dingus.module';
-import { DingusModule } from './dingus/dingus.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost/nest'),
     GamesModule,
-    DingusModule,
   ],
   controllers: [AppController],
   providers: [AppService],
