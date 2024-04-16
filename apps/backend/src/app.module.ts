1da5b98b42dc75be0be5665b64cd0648755de684
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-16 17:33:39 +1000

    add mongoos connection

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/game-review'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
