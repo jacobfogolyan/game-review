72f10c60c50785976a19bb6b2c6ea3b4623dbc68
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-20 23:46:51 +1000

    init game rest api resource

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    GameModule,
   ],
   controllers: [AppController],
   providers: [AppService],
