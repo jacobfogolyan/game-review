904b268fbbb3ec225f1528382eb6d0ab84951a27
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-21 00:02:13 +1000

    chore: init game resource

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GameModule } from './game/game.module';
+import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
