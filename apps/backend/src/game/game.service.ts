7abb5ae8186ac767f525544b1b9f44f0e89e632b
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-22 18:33:37 +1000

    chore: hook up game service

--- apps/backend/src/game/game.service.ts
+++ apps/backend/src/game/game.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Game } from './schemas/game.schema';
+import { Model } from 'mongoose';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 
 @Injectable()
 export class GameService {
-  create(createGameDto: CreateGameDto) {
-    return 'This action adds a new game';
+  constructor(@InjectModel(Game.name) private gameModel: Model<Game>) {}
+
+  async create(createGameDto: CreateGameDto): Promise<Game> {
+    const createdGame = new this.gameModel(createGameDto);
+    return createdGame.save();
   }
 
-  findAll() {
-    return `This action returns all game`;
+  async findAll(): Promise<Game[]> {
+    return this.gameModel.find().exec();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} game`;
+    return this.gameModel.find({ _id: id });
   }
 
   update(id: number, updateGameDto: UpdateGameDto) {
-    return `This action updates a #${id} game`;
+    const updatedGame = new this.gameModel(updateGameDto);
+    return updatedGame.updateOne({ _id: id });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} game`;
+    return this.gameModel.deleteOne({ _id: id });
   }
 }
