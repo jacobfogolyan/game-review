13e92328b28e21527b6ec9dab12de7cf73d3a5fa
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-29 21:57:54 +1000

    init review resource (rest)

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GameModule } from './game/game.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     GameModule,
+    ReviewModule,
   ],
   controllers: [AppController],
   providers: [AppService],
