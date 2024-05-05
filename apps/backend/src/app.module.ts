7139bcc19074b17707c6f97d4e1abede9a64ca11
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-05-05 15:25:03 +1000

    chore: init user resource rest api

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GameModule } from './game/game.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     GameModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
