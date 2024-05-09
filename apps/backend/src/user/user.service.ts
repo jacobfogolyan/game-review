dd1a6454f56bd5f09650a9adc100b6e272568dce
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-05-10 00:04:02 +1000

    chore: add user module to service

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto, UpdateUserDto } from './dto/user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
   create(createUserDto: CreateUserDto) {
-    return this.create(createUserDto);
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 
   findAll() {
-    return this.findAll();
+    return this.userModel.find().exec();
   }
 
-  findOne(id: number) {
-    return this.findOne(id);
+  findOne(id: string) {
+    return this.userModel.find({ _id: id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return this.update(id, updateUserDto);
+    const updatedUser = new this.userModel(updateUserDto);
+    return updatedUser.updateOne({ _id: id }, updateUserDto);
   }
 
   remove(id: number) {
-    return this.remove(id);
+    return this.userModel.deleteOne({ _id: id });
   }
 }
