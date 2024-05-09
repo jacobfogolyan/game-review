978117b4daed9a26c960ab29df46a357e5373613
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-05-09 22:00:35 +1000

    chore: add user schema

---
+++ apps/backend/src/user/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true, type: [String] })
+  permissions: string[];
+
+  @Prop({ default: [] })
+  members: number[];
+
+  @Prop({ default: '' })
+  displayPicture: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
