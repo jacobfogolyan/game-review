19155ba01b162566aa7d4ccda25275be189e9ff7
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-07-18 17:21:00 +1000

    fix: article import paths

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
-import { GenericCrudService } from 'src/generic-crud/generic-crud.service';
+import { GenericCrudService } from '../generic-crud/generic-crud.service';
 
 @Injectable()
 export class UserService extends GenericCrudService<UserDocument> {
