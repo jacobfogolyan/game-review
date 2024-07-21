b6abd579cb8886c525548503888661dc77f955c6
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-07-21 21:33:30 +1000

    chore: add bcrypt to user create method

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -3,11 +3,27 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { GenericCrudService } from '../generic-crud/generic-crud.service';
+import { UserDto } from './dto/user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService extends GenericCrudService<UserDocument> {
   constructor(@InjectModel(User.name) readonly userModel: Model<UserDocument>) {
     super(userModel);
+  }
+
+  async create(userDto: UserDto): Promise<UserDocument> {
+    const saltOrRounds = 10;
+    const password = userDto.password;
+
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+
+    const newUser = {
+      ...userDto,
+      password: hashedPassword,
+    };
+
+    return this.userModel.create(newUser);
   }
 
   async findbyUsername({
