ec4b02dad7c325979b922d445d258ed7316410a9
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-07-21 21:00:54 +1000

    chore: add jwt to user resource

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,25 +1,29 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { User } from 'src/user/schemas/user.schema';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
-  // TODO: https://github.com/kelektiv/node.bcrypt.js#readme
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
   async signIn(
     username: string,
     password: string,
-  ): Promise<Partial<User> | undefined> {
-    const user = await this.userService.findbyUsernamePassword({
+  ): Promise<{ access_token: string }> {
+    const user = await this.userService.findbyUsername({
       username,
-      password,
     });
+
     if (user?.password !== password) {
       throw new UnauthorizedException();
     }
-    const { password: userPassword, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+
+    const payload = { sub: user._id, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
