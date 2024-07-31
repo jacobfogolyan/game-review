88fd1fe07e6729f0d723dd4ed52196823dbc74d8
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-08-01 09:46:02 +1000

    chore: add article and user schemas

---
+++ packages/faker/src/schemas/article.ts
@@ -0,0 +1,31 @@
+import { Schema, Document, model, Model } from "mongoose";
+import mongoose from "mongoose";
+
+interface ArticleDocument extends Document {
+  name: string;
+  description: string;
+  releaseDate?: Date;
+  genres?: string[];
+  platforms?: string[];
+  permissions?: string[];
+  user: mongoose.Types.ObjectId;
+  game: mongoose.Types.ObjectId;
+}
+
+const articleSchema = new Schema<ArticleDocument>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  releaseDate: { type: Date },
+  genres: { type: [String] },
+  platforms: { type: [String] },
+  permissions: { type: [String] },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  game: { type: mongoose.Schema.Types.ObjectId, ref: "Game", required: true },
+});
+
+const Article: Model<ArticleDocument> = model<ArticleDocument>(
+  "Article",
+  articleSchema
+);
+
+export { Article, articleSchema, type ArticleDocument };
---
+++ packages/faker/src/schemas/index.ts
@@ -0,0 +1,11 @@
+import { User, type UserDocument, userSchema } from "./user";
+import { Article, type ArticleDocument, articleSchema } from "./article";
+
+export {
+  User,
+  type UserDocument,
+  userSchema,
+  Article,
+  type ArticleDocument,
+  articleSchema,
+};
