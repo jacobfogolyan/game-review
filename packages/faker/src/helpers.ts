45da196d198b296cffa57333c4d7fcb2f5088e94
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-08-04 21:08:05 +1000

    chore(faker): clean up file

--- packages/faker/src/helpers.ts
+++ packages/faker/src/helpers.ts
@@ -1,18 +1,3 @@
-export const generateObjects = <T>(num: number, template: () => T): T[] => {
-  const objects: T[] = [];
-  for (let i = 0; i < num; i++) {
-    objects.push(template());
-  }
-  return objects;
-};
-
-export function getRandomString<T>(randomArray: Array<T>): T {
-  const randomIndex = Math.floor(Math.random() * randomArray.length);
-  const string = randomArray[randomIndex];
-  if (!string) throw new Error("String undefined");
-  return string;
-}
-
 export function getRandomStrings<T>(arr: Array<T>, num: number): T[] {
   if (num > arr.length) {
     throw new Error("number is greater than array length");
