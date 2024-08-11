a8cba48cbd6e98e78e21d75cca0249ee1173e3d1
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-08-11 12:45:22 +1000

    chore(swagger): init

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Game Review Swagger')
+    .setDescription('with swag')
+    .setVersion('1.0')
+    .addTag('game')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('swagger', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(4000);
 }
 bootstrap();
